42eed1bd5211b80bdc295a59fcbf18277d4d2ca7
maaz10904 <maaz10904@gmail.com>
2026-03-06 20:16:02 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+
+app.get("/api/health", (req,res)=> {
+    res.status(200).json({message:"success "});
+});
+
+
+app.listen(3000, ()=> console.log('server is up and running'))
