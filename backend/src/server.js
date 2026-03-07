fcbdd0d8a7e4a433dbd2eaa510038ee9accfbf55
maaz10904 <maaz10904@gmail.com>
2026-03-07 12:35:02 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,22 @@
-import express from "express"
+import express from "express";
+import path from 'path';
+import { ENV } from "./config/env.js"
 
 const app = express()
+
+const __dirname = path.resolve()
 
 app.get("/api/health", (req,res)=> {
     res.status(200).json({message:"success "});
 });
 
+if(ENV.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../admin/dist")))
 
-app.listen(3000, ()=> console.log('server is up and running'))
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../admin","dist","index.html"))
+    })
+}
+
+
+app.listen(ENV.PORT, ()=> console.log('server is up and running'))
