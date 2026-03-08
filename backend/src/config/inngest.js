5d29f0866ae0b43b3a336dd479c3b8251f84e609
maaz10904 <maaz10904@gmail.com>
2026-03-08 17:25:38 +0530

    ai twea

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -2,14 +2,14 @@ import {Inngest} from 'inngest'
 import {connectDB} from "./db.js"
 import User from '../models/user.model.js'
 
-export const inngest = new Inngest({ id: "GetShopping" })
+export const inngest = new Inngest({ id: "GetShopping" , signingKey: process.env.INNGEST_SIGNING_KEY})
 
 const syncUser = inngest.createFunction(
     { id: "Sync User" },
     {event: "clerk.user.created"},
     async ({ event }) => {
         await connectDB();
-        const { email_addresses, first_name, last_name, profile_image_url, id } = event.data
+        const { email_addresses, first_name, last_name, image_url, id } = event.data
         const newUser = {
             clerkId: id,
             email: email_addresses[0].email_address,
