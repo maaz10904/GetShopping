dfeea7214c80aae50e3ae35797e7fa81af64f4f8
maaz10904 <maaz10904@gmail.com>
2026-03-14 21:45:07 +0530

    admin routes added

---
+++ backend/src/middleware/multer.middleware.js
@@ -0,0 +1,28 @@
+import multer from "multer"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const storage = multer.diskStorage({
+    filename: (req,file,cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
+
+const fileFilter = (req,file,cb) => {
+    const allowedTypes = /jpeg|jpg|png|webp/
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase())
+    const mimetype = allowedTypes.test(file.mimetype)
+
+    if(extname && mimetype){
+        cb(null,true)
+    }    else{
+        cb(new Error("Only images are allowed"))
+    }
+}
+export const upload = multer(
+    {
+        storage,
+        fileFilter,
+        limits:{fileSize: 5 * 1024 * 1024}
+    });
+
