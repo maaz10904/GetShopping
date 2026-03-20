b99a6e0a78889c28d46c0de311a33babdd1be409
maaz10904 <maaz10904@gmail.com>
2026-03-20 12:08:37 +0530

    cart-routes

---
+++ backend/src/routes/cart.route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { addToCart, clearCart, getCart, removeFromCart, updateCartItem } from "../controllers/cart.conroller.js";
+
+const router = Router();
+
+router.use(protectRoute);
+
+router.get("/", getCart)
+router.post("/add", addToCart);
+router.put("/productId", updateCartItem);
+router.delete("/productId", removeFromCart);
+router.delete("/", clearCart);
+
+
+
+export default router;
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from 'path';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
+import cors from "cors";
 
 import { functions, inngest } from "./config/inngest.js";
 
@@ -13,6 +14,7 @@ import userRoutes from "./routes/user.route.js"
 import orderRoutes from "./routes/order.route.js"
 import reviewRoutes from "./routes/review.route.js"
 import productRoutes from "./routes/product.route.js"
+import cartRoutes from "./routes/cart.route.js"
 
 const app = express();
 
@@ -20,6 +22,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(clerkMiddleware());
+app.use(cors({origin:ENV.CLIENT_URL, credentials: true}));"}));
 
 app.use("/api/inngest", serve({client: inngest, functions}));
 
@@ -28,6 +31,7 @@ app.use("/api/users",userRoutes);
 app.use("/api/orders",orderRoutes);
 app.use("/api/reviews",reviewRoutes);
 app.use("/api/products",productRoutes);
+app.use("/api/cart",cartRoutes);
 
 
 
